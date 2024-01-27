aee1f2f8c13e326f29f2bba6243baa4f8471d198
Ziliang Zhang <ziliang1121@gmail.com>
2024-01-27 11:13:53 -0800

    init dev for rttrace

---
+++ rttrace/__init__.py
@@ -0,0 +1,12 @@
+from . import rttrace_main
+from .rttrace_main import rttrace
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+__author__ = "Ziliang(Johnson)"
+__copyright__ = "Copyright 2023, Ziliang(Johnson)"
+__license__ = "MIT"
+__maintainer__ = "Ziliang(Johnson)"
+__email__ = "ziliang.zhang@email.ucr.edu"
+__version__ = "1.0"
---
+++ rttrace/rttrace_main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import math
+
+import plotly.figure_factory as ff
+from datetime import datetime, timedelta
+
+# from task import Task
+
+__author__ = "Ziliang(Johnson)"
+__copyright__ = "Copyright 2023, Ziliang(Johnson)"
+__license__ = "MIT"
+__maintainer__ = "Ziliang(Johnson)"
+__email__ = "ziliang.zhang@email.ucr.edu"
+__version__ = "1.0"
+
+def rttrace(csv_path=None):
+    print("welcome to rttrace")
